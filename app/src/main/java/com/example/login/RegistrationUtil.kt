4a629d2a9ee9af3039bb7c15a93677fbf243d79e
ZB <zivbello@gmail.com>
2021-12-03 09:41:04 -0800

    Implemented all validation functions and their respective tests

--- app/src/main/java/com/example/login/RegistrationUtil.kt
+++ app/src/main/java/com/example/login/RegistrationUtil.kt
@@ -2,21 +2,25 @@ package com.example.login
 
 // object keyword makes it so all the functions are static functions
 object RegistrationUtil {
+    var existingUsers = listOf("cosmicFry", "cosmicY", "bob", "alice", "ToadForSteam")
+    var usedEmails = listOf("cosmic@fry.org", "y@cosmic.com", "bob@bob.bob", "alicealice@bob.bob", "ToadForSteam@mgail.com")
+    // You can use listOf<type>() instead of making a list and adding individually
+
     // make sure it is not empty for each function
 
-
-
     fun validateName(name : String) : Boolean {
-        return true
+        return name.isNotEmpty()
     }
 
-    // swear filter
+    // swear filter(?)
     // already taken
     // real name can't be used
     // meet minimum characters
     // other req.
-    fun validateUserName(username : String) : Boolean {
-        return true
+    fun validateUserName(username : String, name: String) : Boolean {
+        if (existingUsers.indexOf(username) >= 0) return false
+        if (username.length < 8) return false
+        return username.indexOf(name) == -1
     }
 
     // make sure password meets security requirements (deprecated standards)
@@ -25,12 +29,20 @@ object RegistrationUtil {
         // at least one capital letter
     // make sure both passwords match
     fun validatePassword(password : String, confirmPassword : String) : Boolean {
-        return true
+        if (password.length < 8) return false
+        if (password.equals(password.lowercase())) return false
+        for (i in password) {
+            if (i in '0'..'9') return password.equals(confirmPassword)
+        }
+        return false
     }
 
     // make sure the email is not used
     // make sure it is in the proper email format: user@domain.tld
     fun validateEmail(email : String) : Boolean {
-        return true
+        if (usedEmails.indexOf(email) >= 0) return false
+        if (email.indexOf("@") == -1 || email.indexOf(".") == -1 || email.indexOf(" ") >= 0) return false
+        if (email.indexOf(".",email.indexOf("@")) == -1) return false
+        return !email.substring(email.indexOf("@") + 1, email.indexOf("@") + 2).equals(".")
     }
 }
--- app/src/test/java/com/example/login/RegistrationUtilTest.kt
+++ app/src/test/java/com/example/login/RegistrationUtilTest.kt
@@ -45,8 +45,97 @@ class RegistrationUtilTest {
 
     @Test
     fun validatePassword_goodMatchingPassword_returnTrue() {
-        var result = RegistrationUtil.validatePassword("32BigSaladsAndSandwiches", "32BigSaladsAndSandwiches")
+        var result = RegistrationUtil.validatePassword("32BigSaladsAndASandwich", "32BigSaladsAndASandwich")
         assertThat(result).isTrue()
     }
 
+    @Test
+    fun validateName_emptyName_returnsFalse() {
+        var result = RegistrationUtil.validateName("")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateName_goodName_returnsTrue() {
+        var result = RegistrationUtil.validateName("Paul Simon")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun validateUserName_emptyUsername_returnsFalse() {
+        var result = RegistrationUtil.validateUserName("", "Pablo")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateUserName_usedUsername_returnsFalse() {
+        var result = RegistrationUtil.validateUserName("ToadForSteam", "Pablo")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateUserName_tooShort_returnsFalse() {
+        var result = RegistrationUtil.validateUserName("AllDone", "Pablo")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateUserName_usesRealName_returnsFalse() {
+        var result = RegistrationUtil.validateUserName("Pablo1234", "Pablo")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateUserName_goodUsername_returnsTrue() {
+        var result = RegistrationUtil.validateUserName("TheLargeMan", "Pablo")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun validateEmail_emptyEmail_returnsFalse() {
+        var result = RegistrationUtil.validateEmail("")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateEmail_usedEmail_returnsFalse() {
+        var result = RegistrationUtil.validateEmail("bob@bob.bob")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateEmail_noAtSymbolOrPeriod_returnsFalse() {
+        var result = RegistrationUtil.validateEmail("saladTongs")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateEmail_noAtSymbol_returnsFalse() {
+        var result = RegistrationUtil.validateEmail("salad.Tongs")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateEmail_noPeriodAfterAtSymbol_returnsFalse() {
+        var result = RegistrationUtil.validateEmail("sal.lad@Tongs")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateEmail_noDomain_returnsFalse() {
+        var result = RegistrationUtil.validateEmail("saladTongs@.com")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateEmail_hasSpace_returnsFalse() {
+        var result = RegistrationUtil.validateEmail("salad Tongs@meme.org")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateEmail_goodEmail_returnsTrue() {
+        var result = RegistrationUtil.validateEmail("sand.wich@yah.net")
+        assertThat(result).isTrue()
+    }
 }
