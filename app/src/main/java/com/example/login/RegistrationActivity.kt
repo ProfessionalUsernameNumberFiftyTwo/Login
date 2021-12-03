81d858c5e583062f67d3f169644e770f727ae628
ZB <zivbello@gmail.com>
2021-12-03 10:02:50 -0800

    Added validity checks to the registration process

--- app/src/main/java/com/example/login/RegistrationActivity.kt
+++ app/src/main/java/com/example/login/RegistrationActivity.kt
@@ -24,22 +24,36 @@ class RegistrationActivity : AppCompatActivity() {
         binding.buttonRegistrationRegister.setOnClickListener {
             // TODO: verify that information entered is valid
 
-            // in a real app, we'd talk to a database her and save the new user
+
+            // in a real app, we'd talk to a database here and save the new user
 
             // return to the login screen and send back the user & password to prefill
             var returnToLoginIntent = Intent().apply {
                 putExtra(LoginActivity.EXTRA_USERNAME, binding.editTextRegistrationUsername.text.toString())
                 putExtra(LoginActivity.EXTRA_PASSWORD, binding.editTextRegistrationPassword.text.toString())
             }
-            if (binding.editTextRegistrationUsername.text.toString().equals("") && binding.editTextRegistrationPassword.text.toString().equals(""))
+            if (!RegistrationUtil.validateName(binding.editTextRegistrationName.text.toString()))
             {
-                setResult(Activity.RESULT_CANCELED, returnToLoginIntent)
+                Toast.makeText(this, "Please add a name.", Toast.LENGTH_SHORT).show()
+            }
+            else if (!RegistrationUtil.validateUserName(binding.editTextRegistrationUsername.text.toString(),binding.editTextRegistrationName.text.toString()))
+            {
+                Toast.makeText(this, "Username is invalid.", Toast.LENGTH_SHORT).show()
+            }
+            else if (!RegistrationUtil.validateEmail(binding.editTextRegistrationEmail.text.toString()))
+            {
+                Toast.makeText(this, "Email is invalid.", Toast.LENGTH_SHORT).show()
+            }
+            else if (!RegistrationUtil.validatePassword(binding.editTextRegistrationPassword.text.toString(),binding.editTextRegistrationConfirmPassword.text.toString()))
+            {
+                Toast.makeText(this, "Password is invalid/Passwords do not match.", Toast.LENGTH_SHORT).show()
             }
             else
             {
                 setResult(Activity.RESULT_OK, returnToLoginIntent)
+                finish()
             }
-            finish()
+
 
         }
     }
